59a68753a141d7b7cb29a6b6d9443cc4cb02f1b5
Max van den Heuvel <max.heuvel@live.nl>
2020-05-18 11:55:36 +0200

    Added negation of formulas and literals + comparison

--- main2.py
+++ main2.py
@@ -34,7 +34,7 @@ class HelloLoader(HelloListener):
             right = self.decorations[ctx.conclusion()[1]]
             terms = [left, right]
             formula_bool = [isinstance(left, ds.Formula), isinstance(right, ds.Formula)]
-            self.decorations[ctx] = ds.Formula(terms, ds.Operator().OR, formula_bool)
+            self.decorations[ctx] = ds.Formula(terms, ds.Operator().AND, formula_bool)
         pass
 
     def exitPremise(self, ctx):
@@ -104,45 +104,74 @@ def parse(input_stream):
 # Rule -> Formulas -> Literals (neg/pos)
 
 
-# Prints (for now) data depth first, brackets first ((a and b) and (c and d) does not work yet)
+# Returns a struct of the negation of the given formula
+def negateForm(formula):
+    neg_form = formula
+    if isinstance(neg_form, ds.Formula):
+        form_bool = neg_form.formula_bool
+        terms = neg_form.terms
+        if form_bool[0]:
+            negateForm(neg_form.terms[0])
+        if form_bool[1]:
+            negateForm(neg_form.terms[1])
+        else:
+            negateLit(terms[0])
+            negateLit(terms[1])
+        if neg_form.operator == "and":
+            neg_form.operator = "or"
+        else:
+            neg_form.operator = "and"
+    else:
+        negateLit(neg_form)
+    return neg_form
 
-def printData(rule_list):
-    for rule in rule_list:
-        premise = rule.premise
-        conclusion = rule.conclusion
-        to_visit = []
-        save_operator = []
-        while True:
-            # print(type(premise))
-            if isinstance(premise, ds.Formula):
-                form_bool = premise.formula_bool
-                if form_bool[0]:  # Left term is a formula
-                    to_visit.append(premise.terms[1])
-                    save_operator.append(premise.operator)
-                    premise = premise.terms[0]
-                elif form_bool[1]:  # Right term is a formula
-                    to_visit.append(premise.terms[0])
-                    save_operator.append(premise.operator)
-                    premise = premise.terms[1]
-                else:
-                    left = getLiteral(premise.terms[0])
-                    right = getLiteral(premise.terms[1])
-                    print(left + " " + premise.operator + " " + right, end='')
-                    if to_visit:
-                        op = save_operator.pop()
-                        print(" " + op + " ", end='')
-                        premise = to_visit.pop()
-                    else:
-                        print("")
-                        break
+
+# Negates the given literal
+def negateLit(literal):
+    if literal.neg:
+        literal.neg = False
+    else:
+        literal.neg = True
+    return
+
+
+# Prints (for now) formula depth first,
+def printData(formula):
+    to_visit = []
+    save_operator = []
+    while True:
+        if isinstance(formula, ds.Formula):
+            form_bool = formula.formula_bool
+            if form_bool[0]:  # Left term is a formula
+                to_visit.append(formula.terms[1])
+                save_operator.append(formula.operator)
+                formula = formula.terms[0]
+            elif form_bool[1]:  # Right term is a formula
+                to_visit.append(formula.terms[0])
+                save_operator.append(formula.operator)
+                formula = formula.terms[1]
             else:
-                if isinstance(premise, ds.Literal):
-                    print(getLiteral(premise), end='')
-                    if to_visit:
-                        premise = to_visit.pop()
-                    else:
-                        print("")
-                        break
+                left = getLiteral(formula.terms[0])
+                right = getLiteral(formula.terms[1])
+                print(left + " " + formula.operator + " " + right, end='')
+                if to_visit:
+                    op = save_operator.pop()
+                    print(" " + op + " ", end='')
+                    formula = to_visit.pop()
+                else:
+                    print("")
+                    break
+        else:
+            if isinstance(formula, ds.Literal):
+                print(getLiteral(formula), end='')
+                if len(to_visit) == 1:
+                    op = save_operator.pop()
+                    print(" " + op + " ", end='')
+                if to_visit:
+                    formula = to_visit.pop()
+                else:
+                    print("")
+                    break
 
 
 # Transforms a literal in the right format
@@ -156,6 +185,65 @@ def getLiteral(term):
     return minus + atom
 
 
+def comp_lit(literal1, literal2):
+    if literal1.atom == literal2.atom:
+        if literal1.neg == literal2.neg:
+            return True
+    return False
+
+
+# Recursive comparison of two formulas, Returns True if equal, False otherwise.
+def comp_form(form1, form2):
+    success = False
+    if isinstance(form1, ds.Formula) and isinstance(form2, ds.Formula):
+        form_bool1 = form1.formula_bool
+        form_bool2 = form2.formula_bool
+        if form_bool1 == form_bool2:
+            if form1.operator == form2.operator:
+                if form_bool1[0]:  # Left terms are formulas
+                    if comp_form(form1.terms[0], form2.terms[0]):
+                        pass
+                else:
+                    success = comp_lit(form1.terms[0], form2.terms[0])
+
+                if form_bool1[1]:  # Right terms are formulas
+                    if comp_form(form1.terms[1], form2.terms[1]):
+                        pass
+                else:
+                    success = comp_lit(form1.terms[1], form2.terms[1])
+
+    elif isinstance(form1, ds.Literal) and isinstance(form2, ds.Literal):
+        success = comp_lit(form1, form2)
+    return success
+
+
+# def pb_to_cb(rule_list):
+#     ri = rule_list.pop()
+#     ri_conc = ri.conclusion
+#     neg_ri_conc = negateForm(ri_conc)
+#
+#     for rj in rule_list[::-1]:  # from ri-1 to r1
+#         rj_conc = rj.conclusion
+#         if comp_form(rj_conc, neg_ri_conc):
+#     #         rj.premise = rj.premise and negateForm(ri.premise)
+#     #         Implement a way to add formulas to existing formulas
+#     pb_to_cb(rule_list) # Recursively convert
+
+
 if __name__ == '__main__':
-    rule_list = parse_string("p <- c and b or (b or d).")
-    printData(rule_list)
+    rule_list = parse_string("p and c and c <- l and (c or d)."
+                             "p and d <- l and (c or d).")
+    # negateForm(rule_list[0].premise)
+    rule1 = rule_list[0]
+    rule2 = rule_list[1]
+    prem1 = rule1.premise
+    prem2 = rule2.premise
+    conc1 = rule1.conclusion
+    conc2 = rule2.conclusion
+    # if comp_form(conc1, conc2):
+    #     print("conc kek")
+    if comp_form(prem1, prem2):
+        print("prem kek")
+    # printData(conclusion)
+    printData(prem1)
+    printData(prem2)
